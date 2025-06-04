c2cf7254d247aa372e9e92f00a32cf00a9726995
Artem Demchyshyn <132658418+DemchaAV@users.noreply.github.com>
2025-06-04 14:44:12 +0100

    Add files via upload

---
+++ example.js
@@ -0,0 +1,26 @@
+[
+    {
+        id: "time-for-tea",
+        name: "Time for Tea",
+
+        category: "bombay",
+
+        method: "Wet/dry Shake",
+        glass: "Savage Pony",
+        ice: "no ice",
+        garnish: "Peychauds bitters hearts",
+
+        ingredients: [
+
+            {name: "Bombay", qty: "50ml"},
+            {name: "Lemon Juice", qty: "15ml"},
+            {name: "Earl Grey Sugar Syrup", qty: "15ml"},
+            {name: "Demerara", qty: "5ml"},
+            {name: "Foamer", qty: "1 (pipette/dash assumed)"},
+            {name: "Absinth Spray", qty: "1"}
+
+        ],
+
+        allergens: null
+    }
+]
