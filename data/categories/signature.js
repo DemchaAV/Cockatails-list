44a27f15ce89f14e2e5dd0b62d97c88c4c34efca
Artem Demchyshyn <132658418+DemchaAV@users.noreply.github.com>
2026-02-06 20:37:26 +0000

    Modify Lychee Tini details and registration method
    Updated the Lychee Tini entry and added a new cocktail ID. Adjusted the registration method for cocktails.

--- data/categories/signature.js
+++ data/categories/signature.js
@@ -1,6 +1,6 @@
-// Auto-generated category file
+// Category: signature
 if (typeof window.registerCocktails === 'function') {
-  window.registerCocktails('signature', [
+    window.registerCocktails('signature', [
   {
     "id": "pomelle-spritz",
     "name": "Pomelle Spritz",
@@ -343,8 +343,8 @@ if (typeof window.registerCocktails === 'function') {
     ]
   },
   {
-    "id": "lychee-tini",
-    "name": "Lychee Tini",
+    "id": "lychee-tini-75ml1770409342661",
+    "name": "Lychee Tini 75ml",
     "category": "signature",
     "method": "Stir",
     "glass": "Savage Pony",
@@ -361,13 +361,14 @@ if (typeof window.registerCocktails === 'function') {
       },
       {
         "name": "Manchino Sakura Vermouth",
-        "qty": "75ml"
+        "qty": "TBD"
       },
       {
         "name": "Verjus",
         "qty": "TBD"
       }
-    ]
+    ],
+    "allergens": null
   },
   {
     "id": "savy-tini",
@@ -471,4 +472,501 @@ if (typeof window.registerCocktails === 'function') {
     ]
   }
 ]);
+} else {
+    window.pendingCocktails = window.pendingCocktails || [];
+    window.pendingCocktails.push({ category: 'signature', data: [
+  {
+    "id": "classic-margarita",
+    "name": "Margarita",
+    "category": "signature",
+    "method": "Shake",
+    "glass": "Nick&Nora/Rocks",
+    "ice": "As per glass choice",
+    "garnish": "Salt Rim",
+    "ingredients": [
+      {
+        "name": "Casamigos Blanco",
+        "qty": "50ml"
+      },
+      {
+        "name": "Cointreau",
+        "qty": "20ml"
+      },
+      {
+        "name": "Lime Juice",
+        "qty": "20ml"
+      }
+    ]
+  },
+  {
+    "id": "pomelle-spritz",
+    "name": "Pomelle Spritz",
+    "category": "signature",
+    "method": "Build",
+    "glass": "Wine Glass",
+    "ice": "Ice Cubes",
+    "garnish": "Grapefruit slice",
+    "ingredients": [
+      {
+        "name": "Pomelle Elderflower",
+        "qty": "50ml"
+      },
+      {
+        "name": "FT Soda",
+        "qty": "80ml"
+      },
+      {
+        "name": "Prosseco",
+        "qty": "80ml"
+      }
+    ]
+  },
+  {
+    "id": "twinkle",
+    "name": "Twinkle",
+    "category": "signature",
+    "method": "Shake, top",
+    "glass": "Caprice Coupette",
+    "ice": "No ice",
+    "garnish": "Lemon Zest",
+    "ingredients": [
+      {
+        "name": "Belvedere",
+        "qty": "30ml"
+      },
+      {
+        "name": "Elderflower Cordial",
+        "qty": "20ml"
+      },
+      {
+        "name": "Henriot",
+        "qty": "60ml"
+      }
+    ]
+  },
+  {
+    "id": "white-negroni",
+    "name": "White Negroni",
+    "category": "signature",
+    "method": "Stir",
+    "glass": "Rocks",
+    "ice": "Ice Block",
+    "garnish": "Orange Zest",
+    "ingredients": [
+      {
+        "name": "Monkey 47",
+        "qty": "25ml"
+      },
+      {
+        "name": "Schoefields Dry Vermouth",
+        "qty": "25ml"
+      },
+      {
+        "name": "Luxardo Bitter",
+        "qty": "25ml"
+      }
+    ]
+  },
+  {
+    "id": "singapore-sling",
+    "name": "Singapore Sling",
+    "category": "signature",
+    "method": "Shake",
+    "glass": "Highball",
+    "ice": "Ice Cubes",
+    "garnish": "Orange & Cherry",
+    "ingredients": [
+      {
+        "name": "Tanqueray 10",
+        "qty": "30ml"
+      },
+      {
+        "name": "Pineapple juice",
+        "qty": "50ml"
+      },
+      {
+        "name": "Lime",
+        "qty": "15ml"
+      },
+      {
+        "name": "Cherry Heering",
+        "qty": "15ml"
+      },
+      {
+        "name": "Cointreau",
+        "qty": "7.5ml"
+      },
+      {
+        "name": "Dom Benedictine",
+        "qty": "7.5ml"
+      },
+      {
+        "name": "Grenadine",
+        "qty": "10ml"
+      }
+    ]
+  },
+  {
+    "id": "mai-tai",
+    "name": "Mai Tai",
+    "category": "signature",
+    "method": "Shake",
+    "glass": "Rona Thumbler",
+    "ice": "Cube & Crushed",
+    "garnish": "Dried orange, lime & Mint",
+    "ingredients": [
+      {
+        "name": "RSDC 11yo",
+        "qty": "30ml"
+      },
+      {
+        "name": "RSDC Carta Blanca",
+        "qty": "20ml"
+      },
+      {
+        "name": "Cointreau",
+        "qty": "10ml"
+      },
+      {
+        "name": "Lime",
+        "qty": "20ml"
+      },
+      {
+        "name": "Monin Orgeat",
+        "qty": "10ml"
+      }
+    ]
+  },
+  {
+    "id": "sage-sour",
+    "name": "Sage Sour",
+    "category": "signature",
+    "method": "Shake, dry",
+    "glass": "Rocks",
+    "ice": "Ice Cubes",
+    "garnish": "Sage leaf (1 inside shake)",
+    "ingredients": [
+      {
+        "name": "Silent Pool",
+        "qty": "40ml"
+      },
+      {
+        "name": "Creme de Poire",
+        "qty": "10ml"
+      },
+      {
+        "name": "Pear Puree",
+        "qty": "25ml"
+      },
+      {
+        "name": "Apple Juice",
+        "qty": "25ml"
+      },
+      {
+        "name": "Saline Solution",
+        "qty": "1 pipette"
+      },
+      {
+        "name": "Foamer",
+        "qty": "1 pipette"
+      },
+      {
+        "name": "Black Walnut Bitters",
+        "qty": "1 dash"
+      }
+    ]
+  },
+  {
+    "id": "sc-margarita",
+    "name": "SC Margarita 65ml",
+    "category": "signature",
+    "method": "Shake",
+    "glass": "Nick&Nora/ Rocks",
+    "ice": "No ice (Nick&Nora) / Ice Cubes (Rocks)",
+    "garnish": "Salt Rim",
+    "ingredients": [
+      {
+        "name": "Casamigos Blanco",
+        "qty": "30ml"
+      },
+      {
+        "name": "Cointreau",
+        "qty": "20ml"
+      },
+      {
+        "name": "Lost Exp Espadin",
+        "qty": "5ml"
+      },
+      {
+        "name": "DJ 1942",
+        "qty": "5ml"
+      },
+      {
+        "name": "Muyu Chinotto",
+        "qty": "2.5ml"
+      },
+      {
+        "name": "Lime Juice",
+        "qty": "20ml"
+      }
+    ]
+  },
+  {
+    "id": "sc-paloma",
+    "name": "SC Paloma",
+    "category": "signature",
+    "method": "Shake",
+    "glass": "Highball",
+    "ice": "Cubes",
+    "garnish": "Dehydrated Grapefruit + Raspberry Powder Rim",
+    "ingredients": [
+      {
+        "name": "Don Julio Reposado",
+        "qty": "30ml"
+      },
+      {
+        "name": "Grapefruit Cordial",
+        "qty": "50ml"
+      },
+      {
+        "name": "Lime Juice",
+        "qty": "10ml"
+      },
+      {
+        "name": "Sugar Syrup",
+        "qty": "10ml"
+      },
+      {
+        "name": "Peychauds Bitters",
+        "qty": "1 dash"
+      },
+      {
+        "name": "FT Soda",
+        "qty": "50ml"
+      }
+    ]
+  },
+  {
+    "id": "picante",
+    "name": "Picante",
+    "category": "signature",
+    "method": "Shake",
+    "glass": "Rona Thumbler",
+    "ice": "Cubes",
+    "garnish": "Salt Rim",
+    "ingredients": [
+      {
+        "name": "Patron Silver",
+        "qty": "40ml"
+      },
+      {
+        "name": "Lime juice",
+        "qty": "30ml"
+      },
+      {
+        "name": "Agave Syrup",
+        "qty": "15ml"
+      },
+      {
+        "name": "Chili tincture",
+        "qty": "3 dash"
+      },
+      {
+        "name": "Coriander Bitters",
+        "qty": "2 dash"
+      }
+    ]
+  },
+  {
+    "id": "old-fashioned",
+    "name": "SC Old Fashioned 55ml",
+    "category": "signature",
+    "method": "Stir",
+    "glass": "Rocks",
+    "ice": "Ice Block",
+    "garnish": "Rhubarb Meringue (lemon zest discarded)",
+    "ingredients": [
+      {
+        "name": "JW Black",
+        "qty": "35ml"
+      },
+      {
+        "name": "JW Blue",
+        "qty": "5ml"
+      },
+      {
+        "name": "Rabarbaro Aperetivo",
+        "qty": "5ml"
+      },
+      {
+        "name": "PX Nectar",
+        "qty": "5ml"
+      },
+      {
+        "name": "Merlet Apricot",
+        "qty": "5ml"
+      },
+      {
+        "name": "Demerara Syrup",
+        "qty": "5ml"
+      },
+      {
+        "name": "Peppermint Bitters",
+        "qty": "3 dash"
+      }
+    ]
+  },
+  {
+    "id": "vesper",
+    "name": "Vesper 70ml",
+    "category": "signature",
+    "method": "Royal Shake",
+    "glass": "Caprice Coupette",
+    "ice": "No ice",
+    "garnish": "Lemon Zest",
+    "ingredients": [
+      {
+        "name": "Cygnet 22",
+        "qty": "40ml"
+      },
+      {
+        "name": "Ketel One",
+        "qty": "20ml"
+      },
+      {
+        "name": "Fugit Kina",
+        "qty": "10ml"
+      }
+    ]
+  },
+  {
+    "id": "lychee-tini-75ml1770409342661",
+    "name": "Lychee Tini 75ml",
+    "category": "signature",
+    "method": "Stir",
+    "glass": "Savage Pony",
+    "ice": "No ice",
+    "garnish": "Grape",
+    "ingredients": [
+      {
+        "name": "Grey Goose La Poire",
+        "qty": "TBD"
+      },
+      {
+        "name": "Briottet Lychee",
+        "qty": "TBD"
+      },
+      {
+        "name": "Manchino Sakura Vermouth",
+        "qty": "TBD"
+      },
+      {
+        "name": "Verjus",
+        "qty": "TBD"
+      }
+    ],
+    "allergens": null
+  },
+  {
+    "id": "savy-tini",
+    "name": "Savy Tini",
+    "category": "signature",
+    "method": "Stir",
+    "glass": "Caprice Coupette",
+    "ice": "No ice",
+    "garnish": "Martini Olives",
+    "ingredients": [
+      {
+        "name": "Ketel One",
+        "qty": "30ml"
+      },
+      {
+        "name": "Tanqueray 10",
+        "qty": "20ml"
+      },
+      {
+        "name": "Italicus",
+        "qty": "15ml"
+      },
+      {
+        "name": "Tomato",
+        "qty": "10ml"
+      },
+      {
+        "name": "Balsamic Vinegar",
+        "qty": "2ml"
+      }
+    ]
+  },
+  {
+    "id": "scotch-and-honey",
+    "name": "Scotch & Honey 50ml",
+    "category": "signature",
+    "method": "Stir",
+    "glass": "Savage Low Ball",
+    "ice": "Ice Block",
+    "garnish": "Lemon coin",
+    "ingredients": [
+      {
+        "name": "Macallan 12yo",
+        "qty": "20ml"
+      },
+      {
+        "name": "Peat Monster",
+        "qty": "20ml"
+      },
+      {
+        "name": "St. Germain",
+        "qty": "10ml"
+      },
+      {
+        "name": "Lemon Juice",
+        "qty": "5ml"
+      },
+      {
+        "name": "Honey",
+        "qty": "10ml"
+      }
+    ]
+  },
+  {
+    "id": "cherry-collins",
+    "name": "Cherry Collins 45ml",
+    "category": "signature",
+    "method": "Shake",
+    "glass": "Savage Highball",
+    "ice": "Cubes",
+    "garnish": "Cherry on skewer",
+    "ingredients": [
+      {
+        "name": "Ciroc",
+        "qty": "25ml"
+      },
+      {
+        "name": "Bellamie Cherry",
+        "qty": "20ml"
+      },
+      {
+        "name": "Grapefruit Cordial",
+        "qty": "15ml"
+      },
+      {
+        "name": "Lime Juice",
+        "qty": "5ml"
+      },
+      {
+        "name": "Angostura bitters",
+        "qty": "1 dash"
+      },
+      {
+        "name": "Bobs Chocolate bitters",
+        "qty": "1 dash"
+      },
+      {
+        "name": "FT Raspberry & Orange Soda",
+        "qty": "70ml"
+      }
+    ]
+  }
+] });
 }
